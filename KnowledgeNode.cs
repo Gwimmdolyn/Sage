95fcb90f61be395045aa8e13116266099ae5390b
Gwimmdolyn <cameronjohnf01@gmail.com>
2025-12-05 20:38:26 +0000

    added code for KnowledgeNode class

--- KnowledgeNode.cs
+++ KnowledgeNode.cs
@@ -8,5 +8,30 @@ namespace Sage
 {
     public class KnowledgeNode
     {
+        public int Id { get; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<int> LinkedNodes { get; }
+
+
+        public KnowledgeNode(int id, string title, string content)
+        {
+            Id = id;
+            Title = title;
+            Content = content;
+            LinkedNodes = new List<int>();
+        }
+
+        public void Edit()
+        {
+            Title = newTitle;
+            Content = newContent;
+        }
+
+        public void AddLink(int id)
+        {
+            if (!LinkedNodes.Contains(id))
+                LinkedNodes.Add(id);
+        }
     }
 }
