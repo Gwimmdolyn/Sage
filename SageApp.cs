3dc07b3d1da0f48077f6076a5ed9be9a259edae8
Gwimmdolyn <cameronjohnf01@gmail.com>
2025-12-05 23:36:56 +0000

    added the start of sage UI

--- SageApp.cs
+++ SageApp.cs
@@ -8,5 +8,34 @@ namespace Sage
 {
     public class SageApp
     {
+        private readonly KnowledgeGraph _graph = new KnowledgeGraph();
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n--- SAGE PKM SYSTEM ---");
+                Console.WriteLine("1. Create Node");
+                Console.WriteLine("2. View Node");
+                Console.WriteLine("3. Edit Node");
+                Console.WriteLine("4. Link Nodes");
+                Console.WriteLine("5. Search");
+                Console.WriteLine("6. List All Nodes");
+                Console.WriteLine("7. Exit");
+                Console.WriteLine("Choose an option: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1": CreateNode(); break;
+                    case "2": ViewNode(); break;
+                    case "3": EditNode(); break;
+                    case "4": LinkNodes(); break;
+                    case "5": SearchNodes(); break;
+                    case "6": ListNodes(); break;
+                    default: Console.WriteLine("Invalid option."); break;
+                }
+            }
+        }
+
     }
 }
